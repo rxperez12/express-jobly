313a7a32da46d361dde51bbdb744f82d2f2e44f1
Andrea Jao <ndrjao@gmail.com>
2024-05-22 11:50:09 -0700

    completed part I

--- jobly/helpers/sql.js
+++ jobly/helpers/sql.js
@@ -1,6 +1,31 @@
 import { BadRequestError } from "../expressError.js";
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+/**
+ * Intake as the first arg as an object w/ properties that the user wishes
+ * to update
+ *
+ * Intake as the second arg an object w/ properties that we allow for update,
+ * where the data param keys are in camelCase but the db naming is in snake_case
+ *
+ * EX INPUT:
+ *    { firstName, lastName, password, email },
+ *    {
+ *      firstName: "first_name",
+ *      lastName: "last_name",
+ *      isAdmin: "is_admin",
+ *     }
+ *
+ * Outputs an object that has a setColumn key which has as a pre-written
+ * string to assign snake_case column names to variable index, and a values
+ * key with an array of corresponding values that the user wishes to update
+ *
+ * EX OUTPUT:
+ *    {
+ *      setCols: 'first_name=$1, last_name=$2, password=$3, email=$4'
+ *      values: { first_name, last_name, password, email }
+ *    }
+ *
+ */
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
@@ -8,7 +33,7 @@ function sqlForPartialUpdate(dataToUpdate, jsToSql) {
 
   // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
   const cols = keys.map((colName, idx) =>
-      `"${jsToSql[colName] || colName}"=$${idx + 1}`,
+    `"${jsToSql[colName] || colName}"=$${idx + 1}`,
   );
 
   return {
---
+++ jobly/helpers/sql.test.js
@@ -0,0 +1,38 @@
+import { describe, test, expect } from "vitest";
+import { sqlForPartialUpdate } from "./sql.js";
+
+
+
+describe("sqlForPartialUpdate", function () {
+  test("works: data passed in", function () {
+    const results = sqlForPartialUpdate(
+      {
+        firstName: "Jane",
+        lastName: "Austen",
+        password: "JA1",
+        email: "ja@gmail.com"
+      }, {
+      firstName: "first_name",
+      lastName: "last_name",
+      isAdmin: "is_admin",
+    });
+
+    expect(results).toEqual({
+      setCols:
+        "\"first_name\"=$1, \"last_name\"=$2, \"password\"=$3, \"email\"=$4",
+      values:
+        ["Jane", "Austen", "JA1", "ja@gmail.com"]
+    });
+  });
+
+  test("error: data not passed in", function () {
+
+    expect(() => sqlForPartialUpdate(
+      {}, {
+      firstName: "first_name",
+      lastName: "last_name",
+      isAdmin: "is_admin",
+    })).toThrowError(/No data/);
+
+  });
+});
